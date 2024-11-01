28852fd949fd6ded67712e50f696a06c5f87abfd
The Hai Nguyen <nguytheh@sheridancollege.ca>
2024-11-01 10:01:44 -0400

    Delegate user asking responsibilyt of MainGame class to Prompter class

---
+++ src/ca/sheridancollege/project/Prompter.java
@@ -0,0 +1,74 @@
+package ca.sheridancollege.project;
+
+import java.util.Scanner;
+
+public class Prompter {
+    public static Scanner scanner = new Scanner(System.in);
+
+    // Ask player to enter the name
+    public static String askPlayerName(){
+        // Ask the player to enter their name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Check valid name
+        while (!InputValidator.isValidName(name)) {
+            System.out.print("Invalid input. Please enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        // Return the player's name
+        return name;
+    }
+
+    // Ask player if they want to play again
+    public static String askPlayerToPLayAgain() {
+        // Ask if the player wants to play again
+        System.out.print("Do you want to play again? (Yes/No): ");
+        String userAnswer = scanner.next().toLowerCase();
+
+        // Check for valid input
+        while (!InputValidator.isYesOrNo(userAnswer)) {
+            System.out.println("Invalid input. Please enter Yes or No: ");
+            userAnswer = scanner.next().toLowerCase();
+        }
+        
+        // Return the user's answer
+        return userAnswer;
+    }
+
+    // Ask player to bet
+    public static void askPlayerBet(MainPlayer player) {
+        // Ask player to bet
+        System.out.print("Enter your bet: ");
+        int bet = scanner.nextInt();
+
+        // Check if the player has enough chips
+        while (!InputValidator.isValidBet(player, bet)) {
+            System.out.print("You only have " + player.getChips() + " chips. Please enter a smaller amount: ");
+            bet = scanner.nextInt();
+        }
+
+        // Print a blank line
+        System.out.println();
+
+        // Set the player's bet
+        player.setBet(bet);
+    }
+
+    // Ask player to hit or stand
+    public static String askPlayerHitOrStand() {
+        // Ask player to hit or stand
+        System.out.print("Hit or Stand?: ");
+        String userChoice = scanner.next().toLowerCase();
+
+        // Check for valid input
+        while (!InputValidator.isHitOrStand(userChoice)) {
+            System.out.println("Invalid input. Please enter Hit or Stand: ");
+            userChoice = scanner.next().toLowerCase();
+        }
+
+        // Return the player's choice
+        return userChoice;
+    }
+}
