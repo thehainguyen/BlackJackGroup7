a4be2ed5e34ded49ca3128e8c70133b57200c1f2
The Hai Nguyen <nguytheh@sheridancollege.ca>
2024-11-27 12:54:02 -0500

    Added standard comments to the MainGame class

--- src/ca/sheridancollege/project/MainGame.java
+++ src/ca/sheridancollege/project/MainGame.java
@@ -1,164 +1,204 @@
 package ca.sheridancollege.project;
 
+/**
+ * MainGame class extends the Game class to implement the logic for a blackjack game.
+ * It handles player and dealer actions, determines winners, and maintains the game flow.
+ * 
+ * @author The Hai Nguyen - 991745555 - November 27, 2024
+ */
 public class MainGame extends Game {
+
     private Dealer dealer; // Dealer object
     private MainPlayer player; // Player object
     private DeckOfCards deckOfCards; // Deck of cards
-    
+
+    /**
+     * Constructor to initialize the MainGame with a name and a deck of cards.
+     * 
+     * @param name The name of the game.
+     */
     public MainGame(String name) {
         super(name); // Set the name of the game
-        this.deckOfCards = new DeckOfCards(); //Create the deck of cards
+        this.deckOfCards = new DeckOfCards(); // Create the deck of cards
     }
 
-    // Start the game
+    /**
+     * Starts the blackjack game. Handles the flow of actions for the player and dealer,
+     * including dealing cards, checking for blackjack, and determining the winner.
+     */
     @Override
     public void play() {
         // Dealer deals 2 cards to player and dealer
         this.dealer.start(this.player, this.deckOfCards);
 
-        // Display player's cards and dealer's cards
-        Displayer.displayAll(this.player, this.dealer); 
+        // Display initial cards of both player and dealer
+        Displayer.displayAll(this.player, this.dealer);
 
-        // Check if player or dealer has blackjack
+        // Check if there's an initial blackjack for player or dealer
         if (isBlackJack()) {
             return;
         }
 
-        // Player's turn
+        // Player's turn to hit or stand
         while (this.player.getScore() <= 21) {
-            // Ask player if they want to hit or stand
+            // Ask player for their choice (hit or stand)
             String userChoice = Prompter.askPlayerHitOrStand();
 
-            // If player hits, deal another card
+            // If player chooses to hit, deal another card
             if (userChoice.equals("hit")) {
                 this.dealer.dealToPlayer(this.player, this.deckOfCards);
-                Displayer.displayAll(this.player, this.dealer);
+                Displayer.displayAll(this.player, this.dealer); // Show updated hands
             } else {
-                break; // Player stands, end turn
-            }    
+                break; // End player's turn
+            }
         }
 
-        // If player's score is above 21, declare winner
+        // If player's score exceeds 21, they bust
         if (this.player.getScore() > 21) {
             System.out.println("You Bust!");
-            declarePlayerLose();;
+            declarePlayerLose();
             return;
         }
 
-        // Dealer's turn
-        System.out.println();
-        System.out.println("Dealer's turn...");
-
-        // Dealer's turn
+        // Dealer's turn to play
+        System.out.println("\nDealer's turn...");
         this.dealer.play(this.player, this.deckOfCards);
 
-        // Display player's cards and dealer's cards
+        // Show final hands of both player and dealer
         Displayer.displayAll(this.player, this.dealer);
 
-        // Declare winner
+        // Determine and declare the winner
         declareWinner();
     }
 
-    // Declare winner
+    /**
+     * Determines and declares the winner based on the scores of the player and dealer.
+     */
     @Override
     public void declareWinner() {
-        // If dealer's score is above 21, declare winner
+        // Check if the dealer busts
         if (this.dealer.getScore() > 21) {
             System.out.println("Dealer Bust!");
             declarePlayerWin();
             return;
         }
 
-        // Compare scores if both are below 21
+        // Compare scores if neither busts
         if (this.player.getScore() > this.dealer.getScore()) {
             declarePlayerWin(); // Player wins
         } else if (this.player.getScore() < this.dealer.getScore()) {
-            declarePlayerLose(); // Player loses
+            declarePlayerLose(); // Dealer wins
         } else {
-            Displayer.displayDraw(false); // Draw
+            Displayer.displayDraw(false); // It's a draw
         }
     }
 
-    // Declare player win
+    /**
+     * Declares the player as the winner, updates the player's chips, and displays the result.
+     */
     public void declarePlayerWin() {
-        // Display win amount
-        Displayer.displayWinAmount(this.player); 
-        
-        // Add won amount to player's budget
-        this.player.setChips(this.player.getChips() + this.player.getBet()*2); 
-
-        // Print a blank line
-        System.out.println();
+        Displayer.displayWinAmount(this.player); // Show win amount
+        this.player.setChips(this.player.getChips() + this.player.getBet() * 2); // Update chips
+        System.out.println(); // Blank line for readability
     }
 
-    // Declare player lose
+    /**
+     * Declares the player as the loser, updates the player's chips, and displays the result.
+     */
     public void declarePlayerLose() {
-        // Display lose amount
-        Displayer.displayLoseAmount(this.player);
-
-        // Subtract lost amount from player's budget
-        this.player.setChips(this.player.getChips() - this.player.getBet());
-
-        // Print a blank line
-        System.out.println();
+        Displayer.displayLoseAmount(this.player); // Show lose amount
+        this.player.setChips(this.player.getChips() - this.player.getBet()); // Update chips
+        System.out.println(); // Blank line for readability
     }
 
-    // Check if player or dealer has blackjack
+    /**
+     * Checks if either the player or the dealer has a blackjack.
+     * 
+     * @return true if there is a blackjack, false otherwise.
+     */
     public boolean isBlackJack() {
-        // Both player and dealer have blackjack
+        // Check for a tie blackjack
         if (this.player.getScore() == 21 && this.dealer.getScore() == 21) {
             Displayer.displayDraw(true);
             return true;
-        } 
-        
-        // Player has blackjack
-        if (this.player.getScore() == 21) { 
+        }
+
+        // Check if the player has blackjack
+        if (this.player.getScore() == 21) {
             Displayer.displayPlayerWin(true);
-            declarePlayerWin(); // Player wins
-            return true;
-        } 
-        
-        // Dealer has blackjack
-        if (this.dealer.getScore() == 21) {
-            Displayer.displayDealerWin(true);
-            declarePlayerLose(); // Player loses
+            declarePlayerWin();
             return true;
         }
 
-        // Neither player nor dealer has blackjack
+        // Check if the dealer has blackjack
+        if (this.dealer.getScore() == 21) {
+            Displayer.displayDealerWin(true);
+            declarePlayerLose();
+            return true;
+        }
+
+        // No blackjack
         return false;
     }
-    
-    // Reset the game
+
+    /**
+     * Resets the game by releasing all cards held by the player and dealer.
+     */
     public void playAgain() {
         this.dealer.releaseCards();
         this.player.releaseCards();
     }
 
-    // Player setter and getter
+    /**
+     * Sets the player object.
+     * 
+     * @param player The player object to be set.
+     */
     public void setPlayer(MainPlayer player) {
         this.player = player;
     }
 
+    /**
+     * Gets the player object.
+     * 
+     * @return The current player object.
+     */
     public Player getPlayer() {
         return this.player;
     }
 
-    // Dealer setter and getter 
+    /**
+     * Sets the dealer object.
+     * 
+     * @param dealer The dealer object to be set.
+     */
     public void setDealer(Dealer dealer) {
         this.dealer = dealer;
     }
 
+    /**
+     * Gets the dealer object.
+     * 
+     * @return The current dealer object.
+     */
     public Dealer getDealer() {
         return this.dealer;
     }
 
-    // deckOfCards getter
+    /**
+     * Gets the deck of cards object.
+     * 
+     * @return The deck of cards used in the game.
+     */
     public GroupOfCards getDeckOfCards() {
         return deckOfCards;
     }
 
-    // deckOfCards setter
+    /**
+     * Sets the deck of cards object.
+     * 
+     * @param deckOfCards The deck of cards to be set.
+     */
     public void setDeckOfCards(DeckOfCards deckOfCards) {
         this.deckOfCards = deckOfCards;
     }
