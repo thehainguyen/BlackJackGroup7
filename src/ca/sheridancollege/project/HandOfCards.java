dd4c1d1b227e631cf6cdac500dca663bfb7cb4ed
The Hai Nguyen <nguytheh@sheridancollege.ca>
2024-11-27 13:16:27 -0500

    Added comments to HandOfCards class

--- src/ca/sheridancollege/project/HandOfCards.java
+++ src/ca/sheridancollege/project/HandOfCards.java
@@ -1,12 +1,27 @@
 package ca.sheridancollege.project;
 
+/**
+ * The HandOfCards class models a collection of cards held by a player or dealer. 
+ * It allows cards to be added to the hand and provides a method to reset the hand by clearing the cards.
+ * 
+ * @author The Hai Nguyen - 991745555 - November 27, 2024
+ */
 public class HandOfCards extends GroupOfCards {
-    // Add a card to the group
+
+    /**
+     * Adds a card to the hand of cards.
+     * This method is used to add a card to the player's or dealer's hand.
+     * 
+     * @param card The card to be added to the hand.
+     */
     public void addCard(Card card) {
         getCards().add(card);
     }
 
-    // Reset 
+    /**
+     * Resets the hand by clearing all cards.
+     * This method is used to reset the hand, typically at the start of a new game or round.
+     */
     public void reset() {
         getCards().clear();
     }
