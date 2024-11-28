b2078e73b0dbc11158f41b9bb8ba658b0ec26dda
The Hai Nguyen <nguytheh@sheridancollege.ca>
2024-11-28 09:04:05 -0500

    Added maxChip, winRounds. looseRound to MainPlayer class for statistic

--- src/ca/sheridancollege/project/MainPlayer.java
+++ src/ca/sheridancollege/project/MainPlayer.java
@@ -1,39 +1,47 @@
 package ca.sheridancollege.project;
 
 /**
- * The MainPlayer class models a player in the game who has chips and can place bets.
- * It extends the Player class, adding functionality for tracking the player's chips 
- * and the amount they have bet in the current round. The player starts with an initial
- * chip amount and can update their chips and bet throughout the game.
+ * The MainPlayer class models a player in the game who has chips, can place bets, and tracks
+ * their performance during the game. It extends the Player class, adding attributes and methods
+ * for managing the player's chips, bet, maximum chips, and round statistics (wins and losses).
  * 
- * @author The Hai Nguyen - 991745555 - November 27, 2024
+ * The player starts with an initial chip amount of 1000 and can update these attributes
+ * based on the gameplay.
+ * 
+ * @author The Hai Nguyen
+ * @studentID 991745555
+ * @date November 27, 2024
  */
 public class MainPlayer extends Player {
 
     private int chips = 1000; // The player's current chips
+    private int maxChips = 1000; // The maximum number of chips the player has achieved
     private int bet = 0; // The player's current bet
 
+    private int winRounds = 0; // The number of rounds the player has won
+    private int loseRounds = 0; // The number of rounds the player has lost
+
     /**
-     * Constructor to set the name of the player and initialize their chips and bet.
+     * Constructor to initialize a MainPlayer with a given name.
+     * The player starts with a default chip count of 1000.
      * 
      * @param name The name of the player.
      */
     public MainPlayer(String name) {
-        // Set the name of the player
         super(name);
     }
 
     /**
-     * Gets the current number of chips the player has.
+     * Gets the player's current chip count.
      * 
-     * @return The number of chips the player has.
+     * @return The number of chips the player currently has.
      */
     public int getChips() {
         return chips;
     }
 
     /**
-     * Sets the player's chip count to the specified value.
+     * Updates the player's chip count.
      * 
      * @param chips The new chip count for the player.
      */
@@ -42,9 +50,9 @@ public class MainPlayer extends Player {
     }
 
     /**
-     * Gets the current bet placed by the player.
+     * Gets the player's current bet.
      * 
-     * @return The amount the player has bet.
+     * @return The amount the player has bet in the current round.
      */
     public int getBet() {
         return bet;
@@ -53,9 +61,63 @@ public class MainPlayer extends Player {
     /**
      * Sets the amount the player has bet in the current round.
      * 
-     * @param bet The new bet amount.
+     * @param bet The amount the player wants to bet.
      */
     public void setBet(int bet) {
         this.bet = bet;
+    }
+
+    /**
+     * Gets the maximum number of chips the player has achieved during the game.
+     * 
+     * @return The maximum chips the player has reached.
+     */
+    public int getMaxChips() {
+        return maxChips;
+    }
+
+    /**
+     * Updates the maximum chips value.
+     * 
+     * @param maxChips The new maximum chips value.
+     */
+    public void setMaxChips(int maxChips) {
+        this.maxChips = maxChips;
+    }
+
+    /**
+     * Gets the number of rounds the player has won.
+     * 
+     * @return The number of rounds won by the player.
+     */
+    public int getWinRound() {
+        return winRounds;
+    }
+
+    /**
+     * Updates the number of rounds the player has won.
+     * 
+     * @param winRound The new number of rounds won by the player.
+     */
+    public void setWinRound(int winRound) {
+        this.winRounds = winRound;
+    }
+
+    /**
+     * Gets the number of rounds the player has lost.
+     * 
+     * @return The number of rounds lost by the player.
+     */
+    public int getLoseRound() {
+        return loseRounds;
+    }
+
+    /**
+     * Updates the number of rounds the player has lost.
+     * 
+     * @param loseRound The new number of rounds lost by the player.
+     */
+    public void setLoseRound(int loseRound) {
+        this.loseRounds = loseRound;
     }
 }
